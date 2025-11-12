fdafeeaea829d73de39cef4f051eec9005e035db
Zach <zacharyamassey@gmail.com>
2025-11-12 14:12:32 -0500

    completed MVC

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Web250\Mvc\Controllers;
+
+use Web250\Mvc\Models\Greeting;
+
+class HomeController
+{
+  public function index(): string
+  {
+    $model = new Greeting();
+    $message = $model->hello();
+    ob_start();
+    include __DIR__ . '/../Views/Home.php';
+    return (string) ob_get_clean();
+  }
+}
